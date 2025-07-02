9c608c70cf3d3dac1194693038d432812fd6d398
Amr Elsawalhi <Amr.elsawalhi@gmail.com>
2025-07-02 03:06:07 +0300

    Update export_view.py

--- export_view.py
+++ export_view.py
@@ -3,11 +3,11 @@ from sqlalchemy import create_engine
 import os
 
 # Load credentials from GitHub Actions secrets
-DB_HOST = os.environ["PGHOST"]
-DB_PORT = os.environ["PGPORT"]
-DB_NAME = os.environ["PGDATABASE"]
-DB_USER = os.environ["PGUSER"]
-DB_PASS = os.environ["PGPASSWORD"]
+DB_HOST = os.environ["SUPABASE_DB_HOST"]
+DB_PORT = os.environ["SUPABASE_DB_NAME"]
+DB_NAME = os.environ["SUPABASE_DB_USER"]
+DB_USER = os.environ["SUPABASE_DB_PASSWORD"]
+DB_PASS = os.environ["SUPABASE_DB_PORT"]
 
 # Build connection
 uri = f"postgresql+psycopg2://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
